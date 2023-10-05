f0560008f109f1c2f2d72d1a2a51e427f5d0b372
Jason Carrter <jjcarter@me.com>
2023-10-05 11:48:21 +0100

    Initial commit

---
+++ Test-Shortcut.ps1
@@ -0,0 +1,37 @@
+﻿Function Test-Shortcut
+{
+    [CmdletBinding()]
+	Param
+    (
+        [Parameter(Mandatory=$True, HelpMessage="Please enter Path of Shortcut, Example c:\folder", Position=1)]
+        [string] $path
+    )
+
+    $ErrorActionPreference =' Silentlycontinue'
+
+    $Shortcuts = Get-ChildItem -Recurse $path -Include *.lnk -Force
+    $Shell = New-Object -ComObject WScript.Shell
+
+    Foreach ($Shortcut in $Shortcuts)
+    {
+        $checks = $shell.CreateShortcut($Shortcut).TargetPath   
+     
+        Foreach($check in $checks)
+        {
+            [Pscustomobject]@{ 
+                IsWorking        = Test-Path $($check)
+                Shortcut         = $Shortcut.name
+                ShortcutFullName = $Shortcut.FullName
+                Location         = $($check)
+            }
+        }
+    }
+  }
+
+  # Remove all broken links in the recents folder that point to the I or H drive
+Test-Shortcut -path "${en:USERPROFILE}\AppData\Roaming\Microsoft\Windows\Recent" |
+    Where-Object { $_.IsWorking -eq $false} |
+    Where-Object { $_.Location.Length -gt 0 } |
+    Where-Object { $_.Location.StartsWith('I:\') -or $_.Location.StartsWith('H:\') } |
+    Select-Object ShortcutFullName |
+    ForEach-Object { Remove-Item $_.ShortcutFullName }
